cbb753b309b921cf79603d0adccb4e61414eb91f
avmatys <matys.andrey@gmail.com>
2024-08-05 19:30:23 +0300

    Solved 2053 task with Map and 2 Sets

---
+++ array/2053_kthdiststr/main.py
@@ -0,0 +1,69 @@
+from typing import List
+from datetime import datetime
+
+
+# Track execution time of the function
+def timeit(func):
+    def wrapper(*args, **kwargs):
+        start = datetime.now()
+        value = func(*args, **kwargs)
+        end = datetime.now()
+        print(f"Time: {end-start}")
+        return value
+    return wrapper
+
+
+# https://leetcode.com/problems/kth-distinct-string-in-an-array/description/?envType=daily-question&envId=2024-08-05
+class Solution:
+
+    @timeit
+    def kthDistinct(self, arr: List[str], k: int) -> str:
+        str_count = {}
+        for elem_str in arr:
+            if elem_str in str_count:
+                str_count[elem_str] += 1
+            else:
+                str_count[elem_str] = 1
+        
+        kth_str = ""
+        for elem_str in arr:
+            if str_count[elem_str] == 1:
+                k -= 1
+            if k == 0:
+                kth_str = elem_str
+                break
+        return kth_str
+
+    @timeit
+    def kthDistinctTwoSets(self, arr: List[str], k: int) -> str:
+
+        unique_set = set()
+        unique_set.add(arr[0])
+        non_unique_set = set()
+
+        for i in range(1, len(arr)):
+            if arr[i] in unique_set:
+                unique_set.remove(arr[i])
+                non_unique_set.add(arr[i])
+            elif arr[i] not in non_unique_set:
+                unique_set.add(arr[i])
+
+        kth_str = ""
+        for i in range(len(arr)):
+            if arr[i] in unique_set:
+                k -= 1
+            if k == 0:
+                kth_str = arr[i]
+                break
+        return kth_str
+
+
+
+
+    
+if __name__ == '__main__':
+    solution = Solution()
+    assert "a" == solution.kthDistinctTwoSets(["d","b","c","b","c","a"], 2)
+    assert "aaa" == solution.kthDistinctTwoSets(["aaa","aa","a"], 1)
+    assert "" == solution.kthDistinctTwoSets(["a","b","a"], 3)
+   
