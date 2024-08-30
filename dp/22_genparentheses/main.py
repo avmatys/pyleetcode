d946b0b221a6de5f72e1a2915481dae01829a365
avmatys <matys.andrey@gmail.com>
2024-08-30 21:51:51 +0300

    Solved problem 22 using iterative approach

--- dp/22_genparentheses/main.py
+++ dp/22_genparentheses/main.py
@@ -18,31 +18,58 @@ class Solution:
 
     @timeit
     def generateParenthesis(self, n: int) -> List[str]:
-        
+
         if n <= 0:
             return []
         
         result = []
 
         def generate(open_count: int, close_count: int, sequence: str):
-            
-            print(f"{open_count} {close_count} {sequence}")
 
             # Generation finished
             if open_count == close_count == n:
                 result.append(sequence)
                 return 
             
-            # Start sequence generation
+            # Generate open bracket
             if open_count < n:
                 generate(open_count + 1, close_count, sequence + "(")
 
-            print(f"Second if {open_count} {close_count} {sequence}")
-            
+            # Generate close bracket
             if close_count < open_count:
                 generate(open_count, close_count + 1, sequence + ")")
         
         generate(0, 0, "")
+        
+        return result
+    
+    @timeit
+    def generateParenthesisIterative(self, n: int) -> List[str]:
+        
+        if n <= 0:
+            return []
+        
+        result = []
+        queue = deque()
+        queue.append((0, 0, ""))
+
+        while len(queue) > 0:
+            
+            # Get entry
+            open, close, sequence = queue.popleft()
+            
+            # Append finished str to the result array
+            if open == close == n:
+                result.append(sequence)
+            
+            # Continue processing - prepare new tasks
+            if open < n:
+                queue.append((open + 1, close, sequence + "("))
+
+            # We can try to close brackets
+            if close < open:
+                queue.append((open, close + 1, sequence + ")"))
+
         return result
     
 
@@ -60,7 +87,7 @@ if __name__ == '__main__':
     for case in cases:
         input = case[0]
         expected = case[1]
-        judge(solution.generateParenthesis(input), expected)
+        judge(solution.generateParenthesisIterative(input), expected)
 
     
    
