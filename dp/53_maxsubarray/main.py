488b3d88282febd3a5537a6c4b34fab0ce608462
avmatys <matys.andrey@gmail.com>
2024-08-31 21:04:12 +0300

    Solved problem 53

---
+++ dp/53_maxsubarray/main.py
@@ -0,0 +1,49 @@
+from typing import List
+from datetime import datetime
+from collections import deque
+
+# Track execution time of the function
+def timeit(func):
+    def wrapper(*args, **kwargs):
+        start = datetime.now()
+        value = func(*args, **kwargs)
+        end = datetime.now()
+        print(f"Time: {end-start}")
+        return value
+    return wrapper
+
+
+# https://leetcode.com/problems/maximum-subarray/description/
+class Solution:
+
+    @timeit
+    def maxSubArray(self, nums: List[int]) -> int:
+        # Store sum of subarray, ending in the position i-th
+        nums_count = len(nums)
+        dp = [0] * nums_count
+        # Set base case
+        dp[0] = nums[0]
+        for i in range(1, nums_count):
+            dp[i] = max(dp[i-1] + nums[i], nums[i]) # We define what is better - add to old subarray or build a new one which is better
+        return max(dp)
+    
+
+def judge(result, expected):
+    print(f'Result {result} Expected {expected}')
+    assert result == expected
+    
+
+if __name__ == '__main__':
+    solution = Solution()
+    cases = [
+                ([-2,1,-3,4,-1,2,1,-5,4], 6),
+                ([1], 1),
+                ([5,4,-1,7,8], 23)
+            ]
+    for case in cases:
+        input = case[0]
+        expected = case[1]
+        judge(solution.maxSubArray(input), expected)
+
+    
+   
