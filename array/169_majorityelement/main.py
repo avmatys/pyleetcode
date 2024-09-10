ed3435b5b35e9481d20e0b7ca401240ecf256bcd
avmatys <matys.andrey@gmail.com>
2024-09-10 19:57:32 +0300

    Solved problem 136

--- array/169_majorityelement/main.py
+++ array/169_majorityelement/main.py
@@ -1,7 +1,6 @@
 from typing import List
-from typing import Optional
 from datetime import datetime
-from collections import deque
+
 
 # Track execution time of the function
 def timeit(func):
@@ -13,15 +12,23 @@ def timeit(func):
         return value
     return wrapper
 
-# https://leetcode.com/problems/single-number/description
+# https://leetcode.com/problems/majority-element/description
 class Solution:
 
     @timeit
-    def singleNumber(self, nums: List[int]) -> int:
-        result = 0
+    def majorityElement(self, nums: List[int]) -> int:
+        # Boyer Moore Majority Algorithm
+        counter = 0
+        majority_element = 0
         for num in nums:
-            result ^= num
-        return result
+            if counter == 0:
+                majority_element = num
+                counter += 1
+            elif majority_element == num:
+                counter += 1
+            else:
+                counter -= 1
+        return majority_element
         
 
 def judge(result, expected):
@@ -32,13 +39,13 @@ def judge(result, expected):
 if __name__ == '__main__':
     solution = Solution()
     cases = [
-        ([2,2,1], 1),
-        ([4,1,2,1,2], 4)
+        ([2,2,1], 2),
+        ([4,1,2,2,1,2], 2)
     ]
     for case in cases:
         input = case[0]
         expected = case[1]
-        judge(solution.singleNumber(input), expected)
+        judge(solution.majorityElement(input), expected)
 
     
    
