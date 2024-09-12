b3d4f5bf08aaa79a3aaac061fc2bd63df7a7395c
avmatys <matys.andrey@gmail.com>
2024-09-12 20:47:53 +0300

    Solved problem 303

---
+++ array/303_rangesumquery/main.py
@@ -0,0 +1,27 @@
+from typing import List
+from datetime import datetime
+
+
+# Track execution time of the function
+def timeit(func):
+    def wrapper(*args, **kwargs):
+        start = datetime.now()
+        value = func(*args, **kwargs)
+        end = datetime.now()
+        print(f"Time: {end-start}")
+        return value
+    return wrapper
+
+
+# https://leetcode.com/problems/range-sum-query-immutable
+class NumArray:
+
+    def __init__(self, nums: List[int]):
+        self.nums = nums
+
+    def sumRange(self, left: int, right: int) -> int:
+        return sum(self.nums[left:right+1])
+        
+
+    
+   
