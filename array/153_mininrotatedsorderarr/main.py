a6da9891a35dc85e2ca74251df89aabb05f5f2fa
avmatys <matys.andrey@gmail.com>
2024-12-05 22:54:25 +0300

    Solved problem 153

---
+++ array/153_mininrotatedsorderarr/main.py
@@ -0,0 +1,55 @@
+from typing import List
+from datetime import datetime
+
+
+# Track execution time of the function
+def timeit(func):
+    def wrapper(*args, **kwargs):
+        start = datetime.now()
+        value = func(*args, **kwargs)
+        end = datetime.now()
+        print(f"Time: {end-start}")
+        return value
+    return wrapper
+
+
+# https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/description/
+class Solution:
+
+    def findMin(self, nums: List[int]) -> int:
+        n = len(nums)
+        left = 0
+        right = n - 1
+        while left < right:
+            mid = (left + right) // 2
+            print(f"{nums[left]} {nums[mid]} {nums[right]}")
+            if nums[mid] >= nums[right]:
+                left = mid + 1
+            else:
+                right = mid
+            
+        return nums[left]
+
+
+
+
+def judge(result, expected):
+    print(f'Result {result} Expected {expected}')
+    assert result == expected
+    
+
+if __name__ == '__main__':
+    solution = Solution()
+    cases = [
+        ([3,4,5,1,2], 1),
+        ([4,5,6,7,0,1,2], 0),
+        ([11,13,15,17], 11),
+        ([3,1,2], 1)
+    ]
+    for case in cases:
+        input = case[0]
+        expected = case[1]
+        judge(solution.findMin(input), expected)
+
+    
+   
