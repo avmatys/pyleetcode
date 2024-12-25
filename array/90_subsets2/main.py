db3536c7ea8c8745de4f49e9100060bee8b84c24
avmatys <matys.andrey@gmail.com>
2024-12-25 21:35:01 +0300

    Solved problem 90

---
+++ array/90_subsets2/main.py
@@ -0,0 +1,54 @@
+from typing import List
+from datetime import datetime
+
+
+# Track execution time of the function
+def timeit(func):
+    def wrapper(*args, **kwargs):
+        start = datetime.now()
+        value = func(*args, **kwargs)
+        end = datetime.now()
+        print(f"Time: {end-start}")
+        return value
+    return wrapper
+
+
+# https://leetcode.com/problems/subsets-ii/description/
+class Solution:
+
+    @timeit
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        sorted_nums = sorted(nums)
+        size = len(sorted_nums)
+        def generate_subset(subset, start_idx, k):
+            print(f"{subset} {start_idx} {k}")
+            result.append(subset[:])
+            for i in range(start_idx, size):
+                if i == start_idx or nums[i-1] != nums[i]:
+                    subset.append(sorted_nums[i])
+                    generate_subset(subset, i + 1, k + 1)
+                    subset.pop()
+
+        generate_subset([], 0, 0)
+        return result
+     
+
+def judge(result, expected):
+    print(f'Result {result} Expected {expected}')
+    assert result == expected
+    
+
+if __name__ == '__main__':
+    solution = Solution()
+    cases = [
+                ([1,2,2], [[],[1],[1,2],[1,2,2],[2],[2,2]]),
+                ([0], [[],[0]])
+            ]
+    for case in cases:
+        input = case[0]
+        expected = case[1]
+        judge(solution.subsetsWithDup(input), expected)
+
+   
+    
