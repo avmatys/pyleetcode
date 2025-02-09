8e9ff09682bbba104d9ae6f4c2926f43e742de71
avmatys <matys.andrey@gmail.com>
2025-02-09 23:01:54 +0300

    Solved problem 50

---
+++ recursion/51_nqueens/main.py
@@ -0,0 +1,82 @@
+from typing import List
+from datetime import datetime
+
+
+# Track execution time of the function
+def timeit(func):
+    def wrapper(*args, **kwargs):
+        start = datetime.now()
+        value = func(*args, **kwargs)
+        end = datetime.now()
+        print(f"Time: {end-start}")
+        return value
+    return wrapper
+
+
+# https://leetcode.com/problems/n-queens/description/
+class Solution:
+
+    def solveNQueens(self, n: int) -> List[List[str]]:
+
+        # Helper method to check if board is valid with a new added queen
+        def is_board_valid(board, row, col):
+            # Check row to the left
+            i, j = row, 0
+            while j < col:
+                if board[i][j] == 'Q':
+                    return False
+                j += 1
+            # Left top diag
+            i, j = row, col
+            while i >= 0 and j >=0:
+                if board[i][j] == 'Q':
+                    return False
+                i, j = i - 1, j - 1
+            # Left bottom diag
+            i, j = row, col
+            while i < n and j >= 0:
+                if board[i][j] == 'Q':
+                    return False
+                i, j = i + 1, j - 1
+            return True
+
+        # Main method to solve a task using a backtracking
+        def solve(board, col, result):
+            if col == n:
+                result.append([''.join(row) for row in board])
+                return
+            for i in range(n):
+                if is_board_valid(board, i, col):
+                    board[i][col] = 'Q'
+                    solve(board, col + 1, result)
+                    board[i][col] = '.'
+                
+        # Call main function
+        board = [['.'] * n for _ in range(n)]
+        result = []
+        solve(board, 0, result)
+        return result
+
+
+def judge(result: str, expected: str):
+    print(f'Result:   {result}')
+    print(f'Expected: {expected}')
+    assert len(result) == len(expected)
+    for exp in expected:
+        assert exp in result
+    
+
+if __name__ == '__main__':
+    solution = Solution()
+    cases = [
+        (4, [[".Q..","...Q","Q...","..Q."],["..Q.","Q...","...Q",".Q.."]]),
+        (1, [["Q"]]),
+        (2, [])
+    ]
+    for case in cases:
+        input = case[0]
+        expected = case[1]
+        judge(solution.solveNQueens(input), expected)
+
+    
+   
