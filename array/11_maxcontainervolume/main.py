fb16c407de8b84223ae1e852c19151638cc292f9
Andrei Matys <Avmatys@ac.atlantconsult.com>
2025-03-03 13:23:34 +0200

    resolved problem 11

--- array/11_maxcontainervolume/main.py
+++ array/11_maxcontainervolume/main.py
@@ -42,6 +42,19 @@ class Solution:
         return max_area
     
 
+class SolutionRev:
+    def maxArea(self, height: List[int]) -> int:
+        left, right = 0, len(height) - 1
+        max_volume = 0
+        while left < right:
+            max_volume = max(max_volume, min(height[left], height[right]) * (right - left))
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+        return max_volume
+
+
 def judge(result, expected):
     print(f'Result {result} Expected {expected}')
     assert result == expected
