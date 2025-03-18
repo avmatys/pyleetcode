2ee18b1350b09b0be415fa0a9a01732aed0d9f99
avmatys <matys.andrey@gmail.com>
2025-03-18 23:02:06 +0300

    solved problem 901

---
+++ collection/901_onlinestockspan/main.py
@@ -0,0 +1,27 @@
+from typing import List
+from datetime import datetime
+
+
+# Track execution time of the function
+def timeit(func):
+    def wrapper(*args, **kwargs):
+        start = datetime.now()
+        value = func(*args, **kwargs)
+        end = datetime.now()
+        print(f"Time: {end-start}")
+        return value
+    return wrapper
+
+
+# https://leetcode.com/problems/online-stock-span/description/
+class StockSpanner:
+
+    def __init__(self):
+        self.stack = []
+
+    def next(self, price: int) -> int:
+        count = 1
+        while self.stack and self.stack[-1][0] <= price:
+            count += self.stack.pop()[1]
+        self.stack.append((price, count))
+        return count
