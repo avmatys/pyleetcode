089e6660e9d70e869bc06be33c8e0d167967c063
avmatys <matys.andrey@gmail.com>
2025-03-18 23:23:50 +0300

    solved problem 1143

---
+++ dp/1143_longestsommonsubseq/main.py
@@ -0,0 +1,42 @@
+from typing import List
+from datetime import datetime
+
+
+# Track execution time of the function
+def timeit(func):
+    def wrapper(*args, **kwargs):
+        start = datetime.now()
+        value = func(*args, **kwargs)
+        end = datetime.now()
+        print(f"Time: {end-start}")
+        return value
+    return wrapper
+
+
+# https://leetcode.com/problems/longest-common-subsequence/
+class Solution:
+
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        dp = [[0 for _ in range(len(text2) + 1)] for _ in range(len(text1) + 1)]
+        for i, ch in enumerate(text1):
+            for j, ch in enumerate(text2):
+                if text1[i] == text2[j]:
+                    dp[i+1][j+1] = dp[i][j] + 1
+                else:
+                    dp[i+1][j+1] = max(dp[i][j+1], dp[i+1][j])
+        return dp[-1][-1]
+
+def judge(result, expected):
+    print(f'Result {result} Expected {expected}')
+    assert result == expected
+    
+
+if __name__ == '__main__':
+    solution = Solution()
+    cases = [
+        (("abcde", "ace"), 3)
+    ]
+    for case in cases:
+        input = case[0]
+        expected = case[1]
+        judge(solution.longestCommonSubsequence(*input), expected)
