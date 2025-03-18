db819809cd4a41ef2afe4367a31042077ee4aea0
avmatys <matys.andrey@gmail.com>
2025-03-18 23:18:35 +0300

    added solution for 338

---
+++ base/338_countbits/main.py
@@ -0,0 +1,42 @@
+from typing import List
+from datetime import datetime
+from collections import deque
+
+# Track execution time of the function
+def timeit(func):
+    def wrapper(*args, **kwargs):
+        start = datetime.now()
+        value = func(*args, **kwargs)
+        end = datetime.now()
+        print(f"Time: {end-start}")
+        return value
+    return wrapper
+
+
+# https://leetcode.com/problems/counting-bits/
+class Solution:
+
+    @timeit
+    def countBits(self, n: int) -> List[int]:
+        dp = [0] * (n + 1)
+        for i in range(1, n + 1):
+            dp[i] = dp[i >> 1] + (i & 1)
+        return dp
+
+def judge(result, expected):
+    print(f'Result {result} Expected {expected}')
+    assert result == expected
+    
+
+if __name__ == '__main__':
+    solution = Solution()
+    cases = [
+        (2, [0,1,1])
+    ]
+    for case in cases:
+        input = case[0]
+        expected = case[1]
+        judge(solution.countBits(input), expected)
+
+    
+   
