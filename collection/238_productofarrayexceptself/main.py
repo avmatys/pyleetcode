2313ef0a254946db48c2bc9e0feace719bab0d1b
avmatys <matys.andrey@gmail.com>
2025-03-19 23:27:52 +0300

    added solution for the 238

--- collection/238_productofarrayexceptself/main.py
+++ collection/238_productofarrayexceptself/main.py
@@ -46,6 +46,18 @@ class SolutionRev:
             suff *= nums[i]
         return result
 
+class SolutionRev2:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        result = [1] * n
+        for i in range(1, n):
+            result[i] = result[i-1] * nums[i-1]
+        accum = 1
+        for i in range(n-1, -1,-1):
+            result[i] *= accum
+            accum *= nums[i]
+        return result
+
 
 def judge(result, expected):
     print(f'Result {result} Expected {expected}')
