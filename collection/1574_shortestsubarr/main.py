4b6bfd33ec1b2562f0f40a0f0f8b0f81a89dceef
avmatys <matys.andrey@gmail.com>
2025-06-15 11:47:25 +0300

    solved problem 1574

---
+++ collection/1574_shortestsubarr/main.py
@@ -0,0 +1,18 @@
+from typing import List
+
+class Solution:
+    def findLengthOfShortestSubarray(self, arr: List[int]) -> int:
+        n = len(arr)
+        r = n - 1
+        while r > 0 and arr[r - 1] <= arr[r]:
+            r -= 1
+        res = r
+        l = 0
+        while l < r and (l == 0 or arr[l-1] <= arr[l]):
+            while r < n and arr[l] > arr[r]:
+                r += 1
+            res = min(res, r - l - 1)
+            l += 1
+        return res
+
+
