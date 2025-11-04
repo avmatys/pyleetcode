ce28a804dca696fda94177d4fcf52aa5248ea216
Andrei Matys <matys.andrey@gmail.com>
2025-11-04 20:08:31 +0200

    solved problem 3679

---
+++ collection/3679_mindiscards/main.py
@@ -0,0 +1,19 @@
+from typing import List
+
+class Solution:
+    def minArrivalsToDiscard(self, arrivals: List[int], w: int, m: int) -> int:
+        n = len(arrivals)
+        freq = defaultdict(int)
+        res = 0
+        for i in range(n):
+            # Shrink a window
+            if i >= w:
+                freq[arrivals[i - w]] -= 1 * int(arrivals[i-w] > 0)
+            # Check if should discard
+            if freq[arrivals[i]] == m:
+                arrivals[i] = 0
+                res += 1
+            else:
+                freq[arrivals[i]] += 1
+        return res
+
