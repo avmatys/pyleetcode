bb3880bcd09e598e2d825ad14214c2efa4a28b0a
Andrei Matys <matys.andrey@gmail.com>
2025-11-18 20:59:19 +0200

    solved problem 341

---
+++ graph/341_flattennestediterator/main.py
@@ -0,0 +1,53 @@
+from typing import List
+
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger:
+#    def isInteger(self) -> bool:
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        """
+#
+#    def getInteger(self) -> int:
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        """
+#
+#    def getList(self) -> [NestedInteger]:
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        """
+
+class NestedIterator:
+    def __init__(self, nestedList: [NestedInteger]):
+        self.stack = [[nestedList, 0]]
+
+    def next(self) -> int:
+        if self.hasNext():
+            lst, idx = self.stack[-1]
+            self.stack[-1][1] = idx + 1
+            return lst[idx].getInteger()
+        return None
+
+    def hasNext(self) -> bool:
+        while self.stack:
+            lst, idx = self.stack[-1]
+            if idx >= len(lst):
+                self.stack.pop()
+                continue
+            if lst[idx].isInteger():
+                return True
+            self.stack[-1][1] = idx + 1
+            self.stack.append([lst[idx].getList(), 0])
+        return False
+
+
+
+
+# Your NestedIterator object will be instantiated and called as such:
+# i, v = NestedIterator(nestedList), []
+# while i.hasNext(): v.append(i.next())
